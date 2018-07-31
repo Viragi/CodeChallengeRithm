0e18f861beffca4acee5fe99e446267d81893cc0
KristenLingwood <kristen.lingwood@gmail.com>
2018-07-31 13:58:33 -0700

    title list for movies added

--- src/TitleList.js
+++ src/TitleList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import data from './sample.json';
+
 class TitleList extends Component {
   constructor(props) {
     super(props);
@@ -7,15 +9,41 @@ class TitleList extends Component {
       titles: []
     };
   }
+
+  componentDidMount() {
+    this.getMovieList();
+    console.log(this.state.titles);
+  }
+
+  getMovieList = () => {
+    const movies = [];
+    let i = 0;
+    while (movies.length <= 21) {
+      console.log(data.entries[i]);
+      if (
+        data.entries[i].programType === 'movie' &&
+        data.entries[i].releaseYear >= 2010
+      ) {
+        movies.push({
+          name: data.entries[i].title,
+          image: data.entries[i].images.poster_art.url
+        });
+      }
+      i++;
+    }
+    this.setState({ titles: movies });
+  };
+
   render() {
     const result = this.state.titles.map(a => {
       return (
         <div key={a.id}>
-          {' '}
-          <Title name={a.title} image={a.img} />{' '}
+          <Title name={a.name} image={a.image} />
         </div>
       );
     });
+    console.log('result', result);
+
     return <div> {result}</div>;
   }
 }
